117e68a234a4562c2018024d20513d92bcda7625
louay <belhassenloulou@yahoo.fr>
2023-04-22 09:55:44 +0200

    migrate methods from js to angular + refactoring

---
+++ src/app/components/chart-modal/chart-modal.component.ts
@@ -0,0 +1,150 @@
+import {Component, OnInit, ElementRef, ViewChild, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+import * as d3 from 'd3';
+import {ITemperature} from "../../models/data";
+import {DataService} from "../../services/data.service";
+import {MapperService} from "../../services/mapper.service";
+import {rgb, scaleOrdinal, schemeCategory10} from "d3";
+
+
+
+
+@Component({
+  selector: 'app-chart-modal',
+  templateUrl: './chart-modal.component.html',
+  styleUrls: ['./chart-modal.component.css']
+})
+export class ChartModalComponent implements OnInit {
+  @ViewChild('chart', { static: false })
+  private chartContainer!: ElementRef ;
+  regionData!: ITemperature[] ;
+  listStations !: string[];
+
+
+  // Set the dimensions of the chart
+   margin = {top: 20, right: 50, bottom: 30, left: 20};
+   width = 960 - this.margin.left - this.margin.right;
+   height = 450 - this.margin.top - this.margin.bottom;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
+              private dataService: DataService,
+              private mapperService: MapperService,) {
+
+    this.dataService.getTemperaturePerRegion(data.regionCode).subscribe((weather) => {
+      this.regionData =this.mapperService.weatherToTemperature(weather);
+      this.listStations = this.extractListStation();
+      this.createLineChart();
+    })
+
+
+  }
+  ngOnInit() {
+
+
+  }
+
+
+
+
+
+
+
+  private createLineChart(): void {
+
+
+    // Append the SVG to the chart container
+    const svg = d3.select(this.chartContainer.nativeElement).append('svg')
+      .attr('width', this.width + this.margin.left + this.margin.right)
+      .attr('height', this.height + this.margin.top + this.margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${this.margin.left},${this.margin.top})`);
+
+
+
+
+
+    // Set the scales
+    const x = d3.scaleTime().range([0, this.width]);
+    const y = d3.scaleLinear().range([this.height, 0]);
+    const legendScale = scaleOrdinal()
+      .domain(this.listStations)
+      .range(schemeCategory10);
+
+
+    // Define the line function
+    const line = d3.line<ITemperature>()
+      .x(d => x(new Date(d.date)))
+      .y(d => y(d.temp_avg));
+
+    // Map the data to x and y values
+    x.domain(d3.extent(this.regionData, (d: ITemperature) => new Date(d.date)) as [Date, Date]);
+    // y.domain([0, (d3.max(this.datas, (d: ITemperature) => d.temp_avg ))] as [number, number]);
+    y.domain(this.getMinMaxTemperatures(this.regionData) as [number, number]);
+
+
+    // Append the x axis
+    svg.append('g')
+      .attr('transform', `translate(0,${this.height})`)
+      .call(d3.axisBottom(x));
+
+    // Append the y axis
+    svg.append('g')
+      .call(d3.axisLeft(y));
+
+    // Append the line path
+    const colorScale = scaleOrdinal(schemeCategory10);
+
+    for (const station of this.listStations) {
+      const color = rgb(colorScale(station));
+      svg.append('path')
+        .datum(this.regionData.filter(d => d.station === station))
+        .attr('fill', 'none')
+        .attr('stroke', color.toString())
+        .attr('stroke-width', 1)
+        .attr('d', line);
+    }
+
+    this.addLegend();
+
+  }
+
+  getMinMaxTemperatures( datas: ITemperature[]): [number, number] {
+    const temperatureValues = datas.map(item => item.temp_avg);
+
+    const minTemp = Math.min(...temperatureValues) ;
+
+    const maxTemp =  Math.max(...temperatureValues) ;
+
+    return [minTemp, maxTemp];
+  }
+  private extractListStation() {
+    const listStation = this.regionData.map(item => item.station);
+    return  Array.from(new Set(listStation));
+  }
+
+  private addLegend() {
+    const svg = d3.select(this.chartContainer.nativeElement).select('svg');
+
+    const legendGroup = svg.append('g')
+      .attr('class', 'legend')
+      .attr('transform', `translate(${this.width - 50}, ${this.margin.top})`);
+
+    const legendItems = legendGroup.selectAll('.legend-item')
+      .data(this.listStations)
+      .enter().append('g')
+      .attr('class', 'legend-item')
+      .attr('transform', (d, i) => `translate(0, ${i * 20})`);
+
+    legendItems.append('rect')
+      .attr('width', 15)
+      .attr('height', 15)
+      .attr('fill', d => rgb(schemeCategory10[this.listStations.indexOf(d)]).toString());
+
+    legendItems.append('text')
+      .attr('x', 20)
+      .attr('y', 10)
+      .attr('dy', '.35em')
+      .text(d => d);
+  }
+
+}
---
+++ src/app/components/map/map.component.ts
@@ -0,0 +1,98 @@
+import { Component } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import * as L from "leaflet";
+import {ChartModalComponent} from "../chart-modal/chart-modal.component";
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent {
+
+
+
+  constructor(private dialog: MatDialog) {
+
+  }
+
+  ngOnInit(): void {
+    // Création de la carte centrée sur la
+    var mymap = L.map('map').setView([46.227638, 2.213749], 6);
+
+    // Ajout du fond de carte OpenStreetMap
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      maxZoom: 18
+    }).addTo(mymap);
+
+
+
+
+    // Chargement des données des régions
+    fetch(
+      'https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/regions.geojson'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // Calcul de la valeur maximale de la densité de population
+        var maxPopulationDensity = 0;
+        data.features.forEach((region: any) => {
+          var populationDensity = region.properties.code;
+          if (populationDensity > maxPopulationDensity) {
+            maxPopulationDensity = populationDensity;
+          }
+        });
+
+        // Création d'une fonction de couleur pour la choropleth map
+        function getColor(d: number) {
+          return d > maxPopulationDensity * 0.8
+            ? '#bd0026'
+            : d > maxPopulationDensity * 0.6
+              ? '#f03b20'
+              : d > maxPopulationDensity * 0.4
+                ? '#fd8d3c'
+                : d > maxPopulationDensity * 0.2
+                  ? '#feb24c'
+                  : '#fed976';
+        }
+
+        // Création d'une couche GeoJSON pour les régions avec une couleur de remplissage basée sur la densité de population
+        var regionLayer = L.geoJSON(data, {
+          style: function (feature) {
+            var populationDensity = feature!.properties.code;
+            return {
+              fillColor: getColor(populationDensity),
+              fillOpacity: 0.7,
+              weight: 1,
+              color: 'black'
+            };
+          },
+          onEachFeature:  (feature, layer ) => {
+            layer.on('click', () => {
+              this.openModal(feature);
+            });
+          }
+
+
+        });
+        // Ajout de la couche à la carte
+        regionLayer.addTo(mymap);
+
+      })
+      .catch((error) => console.error(error));
+  }
+
+
+  private openModal<G, P>(feature: any) {
+    const dialogRef = this.dialog.open(ChartModalComponent, {
+
+      data: {
+        regionName: feature.properties.nom,
+        regionCode:feature.properties.code
+      }
+    });
+  }
+}
