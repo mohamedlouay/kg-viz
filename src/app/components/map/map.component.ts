853468d4f6777c8eb4d873854d2f442b02fee1a1
Mokhtar SAMY <60510677+MokhtarSamy@users.noreply.github.com>
2023-05-14 19:37:12 +0200

    UPDATE: parameter filter

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common'
+import { Component, Input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import * as d3 from 'd3';
 import * as hexbin from 'd3-hexbin';
@@ -7,11 +7,12 @@ import { select } from 'd3-selection';
 import * as L from 'leaflet';
 import * as Ld3 from '@asymmetrik/leaflet-d3';
 import { ChartModalComponent } from '../chart-modal/chart-modal.component';
-import { HexbinLayerConfig } from 'leaflet';
+import { GeoJSON, HexbinLayerConfig, Layer } from 'leaflet';
 import { DataService } from '../../services/data.service';
 import { MapperService } from '../../services/mapper.service';
 import { Station } from '../../models/data';
 import { Observable, Subscriber } from 'rxjs';
+import { ParameterFilterComponent } from '../parameter-filter/parameter-filter.component';
 
 @Component({
   selector: 'app-map',
@@ -24,6 +25,11 @@ export class MapComponent {
   options: any;
   hexLayer: any;
   stationsData: Station[] = [];
+  mymap: L.Map ;
+  layer: L.TileLayer;
+  regionLayer: any;
+
+  @Input() activeButton: string | undefined;
 
   private hexbinOptions!: HexbinLayerConfig;
 
@@ -31,87 +37,37 @@ export class MapComponent {
     private dialog: MatDialog,
     private dataService: DataService,
     private mapperService: MapperService
-  ) {}
+  ) {
 
-  ngOnInit(): void {
-    const hexLayout = hexbin.hexbin();
-    this.hexbinOptions = {
-      radius: 10,
-      opacity: 0.7,
-      duration: 500,
-    };
-
-    // Création de la carte centrée sur la
-    var mymap = L.map('map').setView([46.227638, 2.213749], 6);
-
-    // Ajout du fond de carte OpenStreetMap
-    const layer = L.tileLayer(
+   this.mymap = L.map('map').setView([46.227638, 2.213749], 6);
+    this.layer = L.tileLayer(
       'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       {
         attribution:
           'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
         maxZoom: 18,
       }
-    ).addTo(mymap);
+    ).addTo(this.mymap);
+  }
+
+  ngOnInit(): void {
+  console.log("LAYER ", this.layer);
+  console.log("mymaponinit", this.mymap);
+    this.hexbinOptions = {
+      radius: 10,
+      opacity: 0.7,
+      duration: 500,
+    };
+    // Création de la carte centrée sur la
+    // Ajout du fond de carte OpenStreetMap
+
 
     //ajustement de la luminosité de la map
-    layer.getContainer()!.style.filter = 'brightness(75%)';
+    this.layer.getContainer()!.style.filter = 'brightness(75%)';
     // Chargement des données des régions
-    fetch(
-      'https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/regions.geojson'
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        // Calcul de la valeur maximale de la densité de population
-        var maxPopulationDensity = 0;
-        data.features.forEach((region: any) => {
-          var populationDensity = region.properties.code;
-          if (populationDensity > maxPopulationDensity) {
-            maxPopulationDensity = populationDensity;
-          }
-        });
 
-        // Création d'une fonction de couleur pour la choropleth map
-        function getColor(d: number) {
-          return d > maxPopulationDensity * 0.8
-            ? '#bd0327'
-            : d > maxPopulationDensity * 0.6
-            ? '#f03b20'
-            : d > maxPopulationDensity * 0.4
-            ? '#fd8d3c'
-            : d > maxPopulationDensity * 0.2
-            ? '#feb24c'
-            : '#fed976';
-        }
-
-        // Création d'une couche GeoJSON pour les régions avec une couleur de remplissage basée sur la densité de population
-        var regionLayer = L.geoJSON(data, {
-          style: function (feature) {
-            var populationDensity = feature!.properties.code;
-            return {
-              fillColor: getColor(populationDensity),
-              fillOpacity: 0.75,
-              weight: 1,
-              color: 'black',
-            };
-          },
-          onEachFeature: (feature, layer) => {
-            layer.on('click', () => {
-              this.openModal(feature);
-            });
-            layer.bindTooltip(
-              feature.properties.nom,
-              {
-                permanent:false,
-                direction:'center',
-                className: 'regionLabel'
-              });
-          },
-        });
-        // Ajout de la couche à la carte
-        regionLayer.addTo(mymap);
-      })
-      .catch((error) => console.error(error));
+    this.addRegionLayer().then( () => {
+      console.log('region ', this.regionLayer);
 
     // ---------------------query test---------------------
     this.options = {
@@ -119,27 +75,20 @@ export class MapComponent {
       opacity: 0.7,
       duration: 500,
     };
-
     // @ts-ignore
     (this.franceBounds = L.latLngBounds([
       [41, -5],
       [51, 10],
     ])),
-      /*
-    var map = L.map("map", {
-      maxBounds: franceBounds, // Set maximum bounds of the map
-      maxBoundsViscosity: 1.0, // Make sure the user cannot drag the map out of bounds
-    }).setView([47, 2], 5);
-    */
-      this.colorScale = d3
+
+      (this.colorScale = d3
         .scaleLinear()
         .domain([5, 15, 18, 28])
-        .range([0, 5, 10, 30]);
+        .range([0, 5, 10, 30]));
 
-      this.hexLayer = L.hexbinLayer(this.hexbinOptions);
-    this.hexLayer
-      .colorScale(this.colorScale);
-    this.hexLayer.colorRange(["white", "yellow", "orange", "red", "darkred"]);
+    this.hexLayer = L.hexbinLayer(this.hexbinOptions);
+    this.hexLayer.colorScale(this.colorScale);
+    this.hexLayer.colorRange(['white', 'yellow', 'orange', 'red', 'darkred']);
 
     // Use the getData function to access the fully populated data
     this.getData().then((data) => {
@@ -155,15 +104,63 @@ export class MapComponent {
         return d[1];
       })
       .colorValue(function (d: any[]) {
-        return parseInt(d[0]["o"][2]);
+        return parseInt(d[0]['o'][2]);
       })
       .radiusValue(function (d: any[]) {
-        return parseInt(d[0]["o"][2]);
+        return parseInt(d[0]['o'][2]);
       });
-    this.createRainLayer(this.stationsData, mymap);
-
+    //this.createRainLayer(this.stationsData, mymap);
+    // @ts-ignore
+    //this.switchLayer();
+    });
   }
-
+  async addRegionLayer(){
+    return fetch(
+      'https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/regions.geojson'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // Calcul de la valeur maximale de la densité de population
+        var maxPopulationDensity = 0;
+        data.features.forEach((region: any) => {
+          var populationDensity = region.properties.code;
+          if (populationDensity > maxPopulationDensity) {
+            maxPopulationDensity = populationDensity;
+          }
+        });
+        // Création d'une fonction de couleur pour la choropleth map
+        function getColor(d: number) {
+          return d > maxPopulationDensity * 0.8
+            ? '#bd0327'
+            : d > maxPopulationDensity * 0.6
+              ? '#f03b20'
+              : d > maxPopulationDensity * 0.4
+                ? '#fd8d3c'
+                : d > maxPopulationDensity * 0.2
+                  ? '#feb24c'
+                  : '#fed976';
+        }
+        // Création d'une couche GeoJSON pour les régions avec une couleur de remplissage basée sur la densité de population
+        this.regionLayer = L.geoJSON(data, {
+          style: function (feature) {
+            var populationDensity = feature!.properties.code;
+            return {
+              fillColor: getColor(populationDensity),
+              fillOpacity: 0.75,
+              weight: 1,
+              color: 'black',
+            };
+          },
+          onEachFeature: (feature, layer) => {
+            layer.on('click', () => {
+              this.openModal(feature);
+            });
+          },
+        });
+        // Ajout de la couche à la carte
+        this.regionLayer.addTo(this.mymap);
+      })
+  }
   async getData() {
     let data: any[][] = [];
     this.dataService.getTemperaturePerStation().subscribe((weather) => {
@@ -175,8 +172,16 @@ export class MapComponent {
     return data;
   }
 
-  async createRainLayer(stations: any[], mymap:L.Map ){
-   var stationsCoordinates : Station[];
+  switchLayer($event: string) {
+    console.log('event', $event);
+    switch ($event) {
+      case 'temperature':
+        console.log("temp ? : ", this.regionLayer);
+       this.mymap = this.mymap.addLayer(this.regionLayer);
+    }
+  }
+  async createRainLayer(stations: any[], mymap: L.Map) {
+    var stationsCoordinates: Station[];
 
     this.dataService.getTemperaturePerStation().subscribe((weather) => {
       stationsCoordinates = this.mapperService.weatherToStation(weather);
@@ -205,10 +210,12 @@ export class MapComponent {
         regionName: feature.properties.nom,
         regionCode: feature.properties.code,
       },
-      position: {bottom:'0px', top:'20%'},
+      position: { bottom: '0px', top: '20%' },
       panelClass: 'custom-dialog',
     });
   }
 
   protected readonly console = console;
+
+
 }
--- src/app/components/parameter-filter/parameter-filter.component.ts
+++ src/app/components/parameter-filter/parameter-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-parameter-filter',
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./parameter-filter.component.css']
 })
 export class ParameterFilterComponent {
-  activeButton: string ="";
+  activeButton: string = "";
+  @Output() parameterSelected = new EventEmitter<string>();
 
   private parameterList : string[] = [
     'wind',
@@ -16,10 +17,15 @@ export class ParameterFilterComponent {
   ]
    public layerType: string | undefined;
 
+  constructor() {
+
+  }
+
   ngOnInit(){
     this.loadRain();
     this.getButtonClass('rain');
     this.layerType = "station";
+    this.activeButton = 'region'
   }
 
   getButtonClass(name: string) {
@@ -29,18 +35,23 @@ export class ParameterFilterComponent {
   }
   loadTemperature() {
     this.activeButton = 'temperature';
+    this.parameterSelected.emit('temperature');
+    console.log("temp loaded ");
   }
 
   loadWind() {
     this.activeButton = 'wind';
+    this.parameterSelected.emit(this.activeButton);
   }
 
   loadRain() {
     this.activeButton = 'rain';
+    this.parameterSelected.emit(this.activeButton);
   }
 
   loadHumidity() {
     this.activeButton = 'humidity';
+    this.parameterSelected.emit(this.activeButton);
   }
 
 }
